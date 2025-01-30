8050ae0d7e571c489b058b2559081844f0d065c5
Yonathan Wondosen <112022096+yonathan001@users.noreply.github.com>
2025-01-30 14:59:50 +0300

    func

---
+++ function.py
@@ -0,0 +1,14 @@
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
