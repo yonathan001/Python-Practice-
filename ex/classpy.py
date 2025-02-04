02c0f47ec262f66358a804e8fce38e8e0bac6fe9
Yonathan Wondosen <112022096+yonathan001@users.noreply.github.com>
2025-02-04 22:27:12 +0300

    class

--- ex/classpy.py
+++ ex/classpy.py
@@ -7,3 +7,16 @@ p1 = Person("John", 36)
 
 print(p1.name)
 print(p1.age)
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f"{self.name}({self.age})"
+
+p1 = Person("John", 36)
+
+print(p1)
