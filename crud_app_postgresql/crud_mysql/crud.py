68148fcb01eaaa54e1ebfab71d6bdf069fa9dc20
Yonathan Wondosen <112022096+yonathan001@users.noreply.github.com>
2025-02-16 19:18:54 +0300

    insert

--- crud_app_postgresql/crud_mysql/crud.py
+++ crud_app_postgresql/crud_mysql/crud.py
@@ -1,6 +1,12 @@
 import mysql.connector
 mysqldb = mysql.connector.connect(host="localhost", user="root", password="", database="pycrudsql")
 mysqlcursor = mysqldb.cursor()
-mysqlcursor.execute("create table users (id int auto_increment primary key, name varchar(50), email varchar(50))")  # creating table
 
+ # creating table
+mysqlcursor.execute("create table users (id int auto_increment primary key, name varchar(50), email varchar(50))") 
+
+# inserting values
+mysqlcursor.execute("insert into users(name,email) values(yoni,user1@gmail.com)") 
+mysqldb.commit()
+print(mysqlcursor.rowcount, "record inserted") 
 mysqldb.close()
