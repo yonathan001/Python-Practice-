4dced4544a7346b0c8808a0d3d006882f416ddcf
Yonathan Wondosen <112022096+yonathan001@users.noreply.github.com>
2025-02-16 19:07:27 +0300

    createtablesql

--- crud_app_postgresql/crud_mysql/crud.py
+++ crud_app_postgresql/crud_mysql/crud.py
@@ -1,2 +1,6 @@
 import mysql.connector
-mysqldb = mysql.connector.connect(host="localhost", user="root", password="root", database="pycrudsql")
+mysqldb = mysql.connector.connect(host="localhost", user="root", password="", database="pycrudsql")
+mysqlcursor = mysqldb.cursor()
+mysqlcursor.execute("create table users (id int auto_increment primary key, name varchar(50), email varchar(50))")  # creating table
+
+mysqldb.close()
