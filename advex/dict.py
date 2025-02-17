841dd2eb0e5ec331d2c61bf7f93acae991676be6
Yonathan Wondosen <112022096+yonathan001@users.noreply.github.com>
2025-02-17 11:46:42 +0300

    dict2

--- advex/dict.py
+++ advex/dict.py
@@ -1,4 +1,9 @@
-my_dict = { "a": 1, "b": 2, "c": 3 }
+#my_dict = { "a": 1, "b": 2, "c": 3 }
 
-for key, value in my_dict.items():
-    print(key, value)
+#for key, value in my_dict.items():
+   # print(key, value)
+
+capitals = {"ethiopia":"Shger" ,
+                "kenya":"nairobi"}
+    
+print(capitals.get("ethiopia"))
