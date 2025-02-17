e21d4c57ae7ccfe2d7f88ced07b0061cefe8e7f2
Yonathan Wondosen <112022096+yonathan001@users.noreply.github.com>
2025-02-17 11:32:43 +0300

    dict

---
+++ advex/dict.py
@@ -0,0 +1,4 @@
+my_dict = { "a": 1, "b": 2, "c": 3 }
+
+for key, value in my_dict.items():
+    print(key, value)
--- advex/iterables.py
+++ advex/iterables.py
@@ -1,3 +1,7 @@
 fruits = {"apple", "banana", "cherry"}
 for x in fruits:
     print(x)
+
+    name ="bro code"
+    for character in name:
+        print(character, end="")
