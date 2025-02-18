50b40a492aa411fcf1e7c149b68854e0b157f22d
Yonathan Wondosen <112022096+yonathan001@users.noreply.github.com>
2025-02-18 12:32:41 +0300

    end_prac

--- advex/map.py
+++ advex/map.py
@@ -1,6 +1,6 @@
 def c_to_f(temp):
     return (temp * 9/5)+32
 c_temps =[0, 12, 34, 100]
-f_temps =map(c_to_f, c_temps)
-for temp in f_temps:
-    print(temp)
+f_temps = list(map(c_to_f, c_temps))
+
+print(f_temps)
